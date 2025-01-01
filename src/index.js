b6208203a8a82049922f9ddb94c73153ff841578
dhquan <danghongquan198@gmail.com>
2025-01-01 15:14:36 +0700

    add template engines handerbars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require("express"); // tải thư viên express
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const path = require("path");
 const app = express(); // gọi function
+
 const port = 3000;
+
+// http loggerß
+app.use(morgan("combined"));
+
+// Template engine
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
 // Định nghĩa route -> khi đi dến / thì trả về
 app.get("/", (req, res) => {
   // arrow function
-  res.send("Hello World!");
+  res.render("new");
 });
 //127.0.0.1 - local host
 app.listen(port, () => {
